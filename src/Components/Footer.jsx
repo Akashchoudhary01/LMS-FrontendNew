92fa4b30574aed4de4ae328bf36dd4c019a736f3
Akash <akashkrchoudhary007@gmail.com>
2024-12-29 12:13:50 +0530

    Make the about section fully responsive and add links to the footer icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import {BsFacebook , BsGithub, BsInstagram , BsLinkedin , BsTwitterX} from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const currentDate = new Date();
 const year = currentDate.getFullYear();
@@ -7,22 +8,22 @@ const year = currentDate.getFullYear();
 function Footer(){
     return( 
         <>
-        <footer className='relative left-0 bottom-0 h-[10vh] py-2 flex flex-col sm:flex-row items-center justify-between text-gray-100 bg-gradient-to-r from-blue-900 to-blue-600 sm:px-20 '>
+        <footer className='relative left-0 bottom-0 w-full h-[12vh] md:h-[10vh] md:mb-0 sm:h-[10vh] lg:h-[10vh] py-2 flex flex-col sm:flex-row items-center justify-between text-gray-100 bg-gradient-to-r from-blue-900 to-blue-600 px-4 sm:px-10 md:px-20'>
         <section className='text-lg'>
             Copyright {year} | All right Reserved
         </section>
 
         <section className='flex item-center justify-center gap-7 text-2xl text-white'>
-            <a className='hover:text-black transition-all ease-in-out duration-300'> 
-                <BsFacebook/>
+            <a href=' https://www.facebook.com/profile.php?id=100027197927103' target='_blank' className='hover:text-black transition-all ease-in-out duration-300'> 
+                <BsFacebook />
             </a>
-            <a className='hover:text-black transition-all ease-in-out duration-300'> 
+            <a href='https://www.linkedin.com/in/akashChoudhary007/ ' target='_blank' className='hover:text-black transition-all ease-in-out duration-300'> 
                 <BsLinkedin/>
             </a>
-            <a className='hover:text-black transition-all ease-in-out duration-300'> 
+            <a href='https://github.com/Akashchoudhary01' target='_blank' onDurationChange={300} className='hover:text-black transition-all ease-in-out duration-300'> 
                 <BsGithub/>
             </a>
-            <a className='hover:text-black transition-all ease-in-out duration-300'> 
+            <a href='https://www.instagram.com/_akashchoudhary1/' target='_blank' className='hover:text-black transition-all ease-in-out duration-300'> 
                 <BsInstagram/>
             </a>
         </section>
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,53 +1,53 @@
 import HomeLayout from "../Layouts/HomeLayout";
 import aboutMainImages from "../assets/images/aboutMainImage.png";
 import { celebrities } from "../Constents/CelebritiesData";
-import CarousalSlide from '../Components/CarousalSlide'
+import CarousalSlide from "../Components/CarousalSlide";
 
 function AboutUs() {
-   
   return (
     <HomeLayout>
-      <div className="pl-20 pt-20 flex flex-col text-white">
-        {/* hhh */}
-        <div className="flex flex-col md:flex-row items-center gap-5 mx-5 md:mx-10">
-          <section className="w-full md:w-1/2 space-y-10">
-            <h1 className="text-2xl md:text-5xl text-blue-600 font-semibold">
-              Affordable and quality education
+      <div className="pt-20 px-5 lg:px-10 flex flex-col text-white">
+        <div className="flex flex-col md:flex-row md:pl-5 items-center gap-10 mx-auto">
+          {/* Text Section */}
+          <section className="w-full md:w-1/2 space-y-6 text-center md:text-left">
+            <h1 className="text-3xl md:text-5xl text-blue-600 font-semibold">
+              Affordable and Quality Education
             </h1>
-            <p className="text-xl md:text-3xl text-gray-200">
-              Our goal is to provide the affordable and quaity education to the
-              world . Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <p className="text-lg md:text-2xl text-gray-200">
+              Our goal is to provide affordable and quality education to the
+              world. Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Delectus totam placeat reprehenderit. Repellat velit excepturi
-              illo nemo ad aperiam et commodiblanditiis, iure maiores beatae
+              illo nemo ad aperiam et commodi blanditiis, iure maiores beatae
               inventore! Architecto iusto tenetur labore?
             </p>
           </section>
-          <div className="w-full md:w-1/2">
+
+          {/* Image Section */}
+          <div className="w-full md:w-1/2 flex justify-center">
             <img
-              id="testOne"
               style={{
-                filter: "drop-shadow(0px 10px 10px rgb(0,0,0))",
+                filter: "drop-shadow(0px 10px 10px rgba(0,0,0,0.5))",
               }}
-              className="drop-shadow-2xl"
+              className="drop-shadow-2xl max-w-full h-auto"
               src={aboutMainImages}
-              alt="about main image"
+              alt="About main visual"
             />
           </div>
         </div>
+
         {/*  */}
-         {/* Carousel */}
-        <div className="carousel  w-1/2 my-16 m-auto  ">
-          {celebrities && celebrities.map(celebrity => (<CarousalSlide
-            {...celebrity}
-            key={celebrity.slideNumber}
-            totalSlides={celebrities.length}
-         /> ))}
-           
-
-
+        {/* Carousel */}
+        <div className="carousel w-full sm:w-3/4 md:w-2/3 lg:w-1/2 my-10 sm:my-16 md:my-20 m-auto text-center">
+          {celebrities &&
+            celebrities.map((celebrity) => (
+              <CarousalSlide
+                {...celebrity}
+                key={celebrity.slideNumber}
+                totalSlides={celebrities.length}
+              />
+            ))}
         </div>
-        </div>
-    
+      </div>
     </HomeLayout>
   );
 }
