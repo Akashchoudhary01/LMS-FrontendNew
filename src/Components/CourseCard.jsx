8ab7d63f2149cdc11a63fb7682c537fd8a05622e
Akash <akashkrchoudhary007@gmail.com>
2024-12-31 22:43:26 +0530

    Updated CSS and make to course page fully responsive

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -7,11 +7,11 @@ const CourseCard = ({ data }) => {
   return (
     <div
       onClick={() => navigate("/course/description", { state: { ...data } })}
-      className="text-white w-[16rem] h-[430px] shadow-lg rounded-lg cursor-pointer group overflow-hidden bg-zinc-700"
+      className="text-white w-[18rem] h-[430px] shadow-lg lg:p-6 rounded-lg cursor-pointer group overflow-hidden bg-zinc-700"
     >
       <div className="overflow-hidden">
         <img
-          className="h-48 w-full rounded-tl-lg rounded-tr-lg  group-hover:scale-[1.2]  transition-all ease-in-out duration-300 "
+          className="h-48 w-48 rounded-tl-lg rounded-tr-lg  group-hover:scale-[1.2]  transition-all ease-in-out duration-300 "
           src={data?.thumbnail?.secure_url}
           alt="course thumbnail"
         />
@@ -20,7 +20,7 @@ const CourseCard = ({ data }) => {
       {/* course details */}
       <div className="p-3 space-y-1 text-white">
 
-        <h2 className="text-xl font-bold text-yellow-500 line-clamp-2">
+        <h2 className="text-xl text-center m-auto font-bold text-blue-500 line-clamp-2">
           {data?.title || "untitled course"}
         </h2>
         <p className="line-clamp-2">
