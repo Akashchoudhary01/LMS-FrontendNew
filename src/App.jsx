66d8e1a5af2baa57fe9ae41a85c8c3d65d0febff
Akash <akashkrchoudhary007@gmail.com>
2025-01-02 12:50:21 +0530

    Added the course description page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import CourseList from './Pages/CourseList';
 import Login from './Pages/Login';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
+import CourseDescription from './Pages/CourseDescription';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
             <Route path='/courses' element={<CourseList/>}></Route>
             <Route path='/contact' element={<Contact/>}></Route>
             <Route path='/denied' element={<Denied/>}></Route>
+            <Route path='/course/description' element={<CourseDescription/>}></Route>
 
             <Route path='/register' element={<Singup/>}></Route>
             <Route path='/login' element={<Login/>}></Route>
---
+++ src/Pages/CourseDescription.jsx
@@ -0,0 +1,80 @@
+import HomeLayout from "../Layouts/HomeLayout";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function CourseDescription(){
+    const {state} = useLocation();
+
+    const {role , data } = useSelector((state) => state.auth);
+
+    return(
+        <HomeLayout>
+      <div className="min-h-[90vh] pt-12 px-20 flex flex-col items-center justify-center text-white">
+        {/* displaying the course details */}
+        <div className="flex flex-col gap-10 md:grid md:grid-cols-2 md:gap-10 py-10 relative">
+          {/* creating the left side of description box */}
+          <div className="space-y-5">
+            <img
+              className="w-full h-64"
+              src={state?.thumbnail?.secure_url}
+              alt="thumbnail"
+            />
+
+            {/* course details */}
+            <div className="space-y-4">
+              <div className="flex  flex-col items-center justify-between text-xl">
+                <p className="font-semibold">
+                  <span className="text-blue-500 font-bold">
+                    Total Lectures :{" "}
+                  </span>
+                  {state.numberOfLectures}
+                </p>
+                <p className="font-semibold">
+                  <span className="text-blue-500 font-bold">
+                    Instructor :{" "}
+                  </span>
+                  {state.createdBy}
+                </p>
+              </div>
+
+              {/* adding the subscribe button */}
+              {role === "ADMIN" || data?.subscription?.status === "active" ? (
+                <button
+                  onClick={() =>
+                    navigate("/course/displaylectures", {
+                      state: { ...state },
+                    })
+                  }
+                  className="bg-blue-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-blue-500 transition-all ease-in-out duration-300"
+                >
+                  Watch Lectures
+                </button>
+              ) : (
+                <button
+                  onClick={() => navigate("/checkout")}
+                  className="bg-blue-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-blue-500 transition-all ease-in-out duration-300"
+                >
+                  Subscribe to Course
+                </button>
+              )}
+            </div>
+          </div>
+
+          {/* creating the right section of description box */}
+          <div className="space-y-2 text-xl items-center justify-center">
+            <h1 className="text-3xl font-bold text-blue-500 text-center mb-4">
+              {state.title}
+            </h1>
+
+            <p className="text-blue-500 font-bold">Course Description :</p>
+
+            <p>{state.description}</p>
+          </div>
+        </div>
+      </div>
+
+        </HomeLayout>
+    )
+
+}
+export default CourseDescription;
