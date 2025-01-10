c19d1607c3a4b53fd19371f4f081c2f93fa5e986
Akash <akashkrchoudhary007@gmail.com>
2025-01-10 13:03:56 +0530

    redegine the checkout page

--- src/Pages/Payment/Checkout.jsx
+++ src/Pages/Payment/Checkout.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import {Link} from 'react-router-dom'
 import Layout from "../../Layout/Layout";
 import { BiRupee } from "react-icons/bi";
 import { useDispatch, useSelector } from "react-redux";
@@ -79,44 +80,52 @@ const Checkout = () => {
   return (
     <Layout>
       {/* checkout page container */}
-      <form
-        onSubmit={handleSubscription}
-        className="min-h-[90vh] flex items-center justify-center text-white"
-      >
-        {/* checkout card */}
-        <div className="w-80 h-[26rem] flex flex-col justify-center shadow-[0_0_10px_black] rounded-lg relative">
-          <h1 className="bg-yellow-500 absolute top-0 w-full text-center py-4 text-2xl font-bold rounded-tl-lg rounded-tr-lg">
-            Subscription Bundle
-          </h1>
-
-          <div className="px-4 space-y-5 text-center">
-            <p className="text-[17px]">
-              This purchase will allow you to access all the available courses
-              of our platform for{" "}
-              <span className="text-yellow-500 font-bold">1 Year Duration</span>
-              . <br />
-              All the existing and new launched courses will be available to you
-              in this subscription bundle
-            </p>
-
-            <p className="flex items-center justify-center gap-1 text-2xl font-bold text-yellow-500">
-              <BiRupee /> <span>499</span>only
-            </p>
-
-            <div className="text-gray-200">
-              <p>100% refund at cancellation</p>
-              <p>* Terms & Condition Applied</p>
-            </div>
+       <div className="h-[90vh]  flex items-center text-white justify-center">
+            {/* checkout page container */}
+            <form
+              onSubmit={handleSubscription}
+               className="min-h[90vh] mt-5 items-center justify-center text-white"
+        
+            >
+              {/* checkout card */}
+              <div className="w-80 h-[30rem] m-auto text-center flex flex-col justify-center shadow-[0_0_10px_black] relative rounded-lg">
+                <h1 className="bg-blue-500 absolute left-4 top-2 w-[90%] text-center py-4 text-2xl font-bold rounded-badge ">
+                  Subscription Bundle
+                </h1>
+    
+                <div className="px-4 space-y-5 mt-5 text-center">
+                  <p className="text-[1.1rem] mt-8">
+                    This purchase will allow you to access all the available courses
+                    of our platform for{" "}
+                    <span className="text-blue-500 font-bold">1 Year Duration</span>
+                    . <br />
+                    All the existing and new launched courses will be available to
+                    you in this subscription bundle
+                  </p>
+    
+                  <p className="flex items-center justify-center gap-1 text-2xl font-bold text-yellow-500">
+                    <BiRupee /> <span>499</span>only
+                  </p>
+                  <button
+                    type="submit"
+                    className="bg-blue-500 py-3 px-16 hover:bg-blue-400 rounded-lg transition-all ease-in-out duration-300"
+                  >
+                    Buy Now
+                  </button>
+                  <div className="text-gray-200 items-center justify-center">
+                    <p> 100 % refund On Cancellation </p>
+                    <Link to="/about">
+                      {" "}
+                      <p className="text-xs items-center justify-center absolute bottom-2 right-4 text-blue-600">
+                        {" "}
+                        *Terms and Condition applied*{" "}
+                      </p>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
-
-          <button
-            type="submit"
-            className="bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full text-center py-2 text-xl font-bold rounded-bl-lg rounded-br-lg"
-          >
-            Buy Now
-          </button>
-        </div>
-      </form>
     </Layout>
   );
 };
